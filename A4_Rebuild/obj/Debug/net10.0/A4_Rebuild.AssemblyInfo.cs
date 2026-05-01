798cd43a53b41f26c00a9f6c6ee3085321906142
ShadyRacoon <marianbogdan953@yahoo.com>
2026-05-01 20:11:19 +0300

    patch: email and password required for login dto

--- A4_Rebuild/obj/Debug/net10.0/A4_Rebuild.AssemblyInfo.cs
+++ A4_Rebuild/obj/Debug/net10.0/A4_Rebuild.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A4_Rebuild")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f9ba897a9a78ea622ed8499f45c5cb800ffdbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b46d37b2426e42562e01a97a2600700c85ffb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("A4_Rebuild")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A4_Rebuild")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
