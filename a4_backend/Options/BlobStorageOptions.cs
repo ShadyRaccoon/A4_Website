02a3bf5d3d0a868329eb34d2272781824484884b
Azure Pipeline <pipeline@devops.com>
2026-06-08 00:16:09 +0300

    Blob Options class added

---
+++ a4_backend/Options/BlobStorageOptions.cs
@@ -0,0 +1,9 @@
+namespace a4_backend;
+
+public class BlobStorageOptions
+{
+    public string? ConnectionString { get; set; }
+    public string? ContainerName { get; set; }
+    public string? StorageAccountKey { get; set; }
+    public string? StorageAccountName { get; set; }
+}
